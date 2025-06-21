1c40d790205fc69adbb40bc0d2e74b61c457b088
Devanshk2004 <134846636+Devanshk2004@users.noreply.github.com>
2025-06-21 22:31:03 +0530

    rotate

--- Arrays/left_rotate_by_d.py
+++ Arrays/left_rotate_by_d.py
@@ -1,23 +1,24 @@
-def left_rotate(arr, n, d):
-    d = d % n  # Handle cases where d > n
-    temp = arr[:d]  # Store first d elements
+#def left_rotate(arr, n, d):
+#    d = d % n  
+#    temp = arr[:d] 
+#
+#    for i in range(d, n):
+#        arr[i - d] = arr[i]
+#
+#    for i in range(n - d, n):
+#        arr[i] = temp[i - (n - d)]
 
-    # Shift the rest of the array to the left
-    for i in range(d, n):
-        arr[i - d] = arr[i]
+def left_rotate_ez(arr,d):
+    d = d % n
+    arr[:d] = reversed(arr[:d])
+    arr[d:] = reversed(arr[d:])
+    arr[:] = reversed(arr)
 
-    # Copy the temp elements to the end
-    for i in range(n - d, n):
-        arr[i] = temp[i - (n - d)]
-
-
-# Main logic
 n = int(input())
 arr = list(map(int, input().split()))
 d = int(input())
 
-left_rotate(arr, n, d)
+left_rotate_ez(arr, d)
 
-# Output the result
 print("Rotated array:")
 print(*arr)
